8866172e2ae0991ee196022dfc5cd49470d58555
Yashwanth Y S <yashwanthys@Yashwanths-MacBook-Air.local>
2024-10-18 15:26:30 -0400

    added corrected event files

--- scraper/events_scraper.py
+++ scraper/events_scraper.py
@@ -7,11 +7,7 @@ def main():
     all_cmu_events = scrape_events_from_cmu_pages()
     print("Fetched {} events from cmu webpages.".format(len(all_cmu_events)))
 
-    all_pittsburgh_events = scrape_events_from_pittsburgh_pages()
-    print("Fetched {} events from pittsburgh webpages.".format(len(all_pittsburgh_events)))
-
     all_events.extend(all_cmu_events)
-    all_events.extend(all_pittsburgh_events)
 
     with open("raw_data/dump-event_data.json", "w") as f:
         json.dump(all_events, f, indent=4)
